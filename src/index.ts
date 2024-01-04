a6f6893398d38cc39d613eda1a5f4527bc55bbf5
calanfive <camille.lelan@gmail.com>
2024-01-04 09:21:17 +0100

    updated Dockerfile + tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,44 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { Sequelize } from 'sequelize';
+import { Item } from "./Interfaces/item.interface";
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('sqlite::memory:');
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const port = process.env.PORT;
+const database = process.env.DATABASE as string;
+const username = process.env.USERNAME as string;
+const password = process.env.PASSWORD as string;
+const server = process.env.SERVER as string;
+
+const sequelize = new Sequelize(database, username, password, {
+host: server,
+dialect: "postgres",
+dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    },
+    dialectModule: require('pg'),
+});
+
+TryConnection();
+
+const items: Item[] = [];
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+  });
+
+app.listen(8080, () => {
+  console.log("Express server started on port 3000");
+});
 
 async function TryConnection() {
   try {
@@ -17,13 +49,3 @@ async function TryConnection() {
     console.error('Unable to connect to the database:', error);
   }
 }
-  
-const items: Item[] = [];
-
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-  });
-
-app.listen(3000, () => {
-  console.log("Express server started on port 3000");
-});
